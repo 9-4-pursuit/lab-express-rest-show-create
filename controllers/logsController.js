516406209390c5a30a24f2147419458927fbd858
BertCasale <bthebest55@gmail.com>
2023-04-11 22:08:38 -0400

    bonus order

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -3,12 +3,53 @@ const logs = express.Router();
 const logsArray = require("../models/log.js");
 
 logs.get("/", (req, res) => {
-  res.status(202).json(logsArray);
+  const {order, mistakes, lastCrisis} = req.query;
+
+  //
+  if (!order && !mistakes && !lastCrisis) {
+    res.status(202).json(logsArray);
+  } else {
+    const logsArrayCopy = [...logsArray];
+
+    //sort ascending
+    if (order === "asc") {
+      logsArrayCopy.sort((a, b) => {
+        logTitleA = a.title.toLowerCase();
+        logTitleB = b.title.toLowerCase();
+        if (logTitleA < logTitleB) {
+          return -1;
+        } 
+        if (logTitleA > logTitleB) {
+          return 1;
+        }
+        return 0;
+      })
+      res.json(logsArrayCopy);
+    }
+
+    //sort descending
+    if (order === "desc") {
+      logsArrayCopy.sort((a, b) => {
+        logTitleA = a.title.toLowerCase();
+        logTitleB = b.title.toLowerCase();
+        if (logTitleA > logTitleB) {
+          return -1;
+        } 
+        if (logTitleA < logTitleB) {
+          return 1;
+        }
+        return 0;
+      })
+      res.json(logsArrayCopy);
+    }
+
+
+  }
 });
 
 logs.get("/:id", (req, res) => {
   const {id} = req.params;
-
+  
   logsArray[id] ?
   res.status(202).json(logsArray[id]):
   res.redirect("/error");
