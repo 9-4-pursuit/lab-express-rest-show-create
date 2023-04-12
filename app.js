fc1b9959c4e5194a5974bb957dae263b578e1dc6
BertCasale <bthebest55@gmail.com>
2023-04-11 20:59:34 -0400

    make index and error routes

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const app = express();
 
+
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Captain's Log!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page not found");
+});
+
 module.exports = app;
