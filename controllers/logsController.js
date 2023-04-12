fa505e862cde3a463a54b1b730fc40795fb5d81f
BertCasale <bthebest55@gmail.com>
2023-04-11 21:33:38 -0400

    get individual show based on array index

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,4 +6,12 @@ logs.get("/", (req, res) => {
   res.status(202).json(logsArray);
 });
 
+logs.get("/:id", (req, res) => {
+  const {id} = req.params;
+
+  logsArray[id] ?
+  res.status(202).json(logsArray[id]):
+  res.redirect("/error");
+});
+
 module.exports = logs;
