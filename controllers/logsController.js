28812febe406ef7d5be3f6f58365f4f8dda04637
keriarmstrong <khetto@live.com>
2023-04-12 19:54:25 -0400

    completed PUT

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,35 +6,51 @@ const logs = express.Router();
 const logsArray = require('../models/log.js')
 
 // Get All | index route
-logs.get('/', (req, res) =>{
+logs.get('/', (req, res) => {
     res.status(202).json(logsArray)
 })
 
 //Get by ID | individual show
-logs.get('/:id',(req, res) =>{
-const {id} = req.params
-const log = logsArray[id]
-if(log){
-    res.status(202).json(logsArray[id])
-}else {
-    // res.status(404).send(`<a href='/'>Back to Home</a>`)
-    res.redirect('/')
-}
+logs.get('/:id', (req, res) => {
+    const { id } = req.params
+    const log = logsArray[id]
+    if (log) {
+        res.status(202).json(logsArray[id])
+    } else {
+        // res.status(404).send(`<a href='/'>Back to Home</a>`)
+        res.redirect('/')
+    }
 })
 
 //Create | POST
-logs.post('/',(req, res) =>{
+logs.post('/', (req, res) => {
     const newLog = req.body;
     logsArray.push(newLog)
     res.status(202).json(logsArray)
 })
 
-//Delete
+//Delete by ID
 logs.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    // res.json({message: 'delete route'})
-    const removedLogArr = logsArray.splice(id, 1);
-     res.status(202).json(logsArray)
+    const { id } = req.params;
+    if (logsArray[id]) {
+        const removedLogArr = logsArray.splice(id, 1);
+        res.status(202).json(removedLogArr)
+        // return|res can be either the deleted item or the updated array
+    } else {
+        res.status(404).json({ error: `No log with that ${id}` })
+    }
 })
 
+//Put | update
+logs.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updateLog = req.body;
+    if (logsArray[id]) {
+        logsArray[id] = updateLog
+        res.status(202).json(logsArray)
+    } else {
+        res.status(404).json({ error: `No log matches that ${id}` })
+    }
+
+})
 module.exports = logs
