05dc5475525c70e65e0ab3864666caaf9741dc41
SuperNinjaEv <evan0210@gmail.com>
2023-04-11 22:11:06 -0400

    4/6 tests passing: Working on Post, log creation

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -6,7 +6,7 @@ const logsArray = require("../models/logsModel");
 // Then we do our GETS
 logs.get("/", (req, res) => {
     if (res) {
-        res.status(202).json({ success: true, payload: logsArray });
+        res.status(202).json(logsArray);
     } else {
         res.status(400).json({ error: "Model not found" });
     }
@@ -17,9 +17,9 @@ logs.get("/:id", (req, res) => {
     const logs = logsArray[id];
 
     if (logs) {
-        res.status(202).json({ success: true, payload: logsArray });
+        res.status(202).json(logs);
     } else {
-        res.status(404).send(`No log found with ID ${id}.`)
+        res.redirect("/*");
     }
 })
 
