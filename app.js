70b119b7fd7fb8026cebfaeb1d54c4a54ae62959
SamuelBAlba <brayanbalba24@gmail.com>
2023-04-12 20:46:49 -0400

    passed all test

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ const express = require("express")
 const app = express()
 const logsController = require("./controllers/logsController")
 
+
+app.use(express.json()) // parses incoming data
+
+app.use("/logs", logsController)
+
 app.get("/", (req, res) => {
     res.send("welcome to the captain's log")
 })
 
-app.use("/logs", logsController)
-
-
 app.get("*", (req, res) => {
-    res.status(404).json({ error: "page not found"})
+    res.status(404).json({ error: "Page not found"})
 })
+
+
+
 
 module.exports = app
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,11 +2,12 @@ const express = require("express")
 const logs = express.Router()
 const logsArray = require("../models/log.js")
 
-
+// GET all logs
 logs.get("/", (req, res) => {
     res.json(logsArray)
 })
 
+// GET individual log
 logs.get("/:id", (req, res) => {
     const {id} = req.params
     log = logsArray[id]
@@ -15,10 +16,35 @@ logs.get("/:id", (req, res) => {
         res.json(logsArray[id])
     
     }else {
-        res.send(`The log requested is not available <a href="/logs">please browse here</a>`)
+        res.redirect(302, "/*")
 
     }
 
 })
+
+// // Create POST
+
+logs.post("/", (req, res) => {
+   const newLog = req.body
+    logsArray.push(newLog)
+    res.json(logsArray)
+})
+
+// delete
+
+logs.delete('/:id', (req, res) => {
+    const {id} = req.params
+
+    if(logsArray[id]) {
+        const deletedLog = logsArray.splice(id, 1)
+        res.json(deletedLog)
+    } else {
+        res.status(404).json({sucess: false, error: `no log with this id: ${id}`})
+    }
+})
+
+
+
+
 
 module.exports = logs
