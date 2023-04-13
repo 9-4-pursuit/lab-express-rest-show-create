c354c209117945e0e843bac54a61c20dbfbabb21
Ari-So-Irie <arius1500@gmail.com>
2023-04-12 21:21:13 -0400

    Arius' Express REST Show Create Lab Complete

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const logs = express.Router();
+
 const logsArray = require('../models/log');
 
 //validation
-const validateURL = (req, res, next) => {
-    if (
-        req.body.url.substring(0, 7) === "htttp://" ||
-        req.body.url.substring(0, 8) === "https://"
-    ){
-        return next();
-    } else {
-        res 
-        .status(400)
-        .send(`Oops, you forgot to start your url with http:// or https://`)
-    }
-}
+// const validateURL = (req, res, next) => {
+
+//     // //middleware 
+// // app.use((req, res, next) => {
+// //     console.log("this code runs for every request")
+// //     next()
+// // })
+//     if (
+//         req.body.url.substring(0, 7) === "htttp://" ||
+//         req.body.url.substring(0, 8) === "https://"
+//     ){
+//         return next();
+//     } else {
+//         res 
+//         .status(400)
+//         .send(`Oops, you forgot to start your url with http:// or https://`)
+//     }
+// }
 
 
 //index
@@ -24,13 +31,13 @@ logs.get("/", (req,res) => {
 
 //individual show
 logs.get('/:id', async (req, res) => {
-console.log(req.params.id)
+// console.log(req.params.id)
 const { id } = req.params
 const log = logsArray[id]
 if (log) {
 res.status(202).json(log)
 } else {
-    res.status(404).send(` Sorry the index you requested is not hosted on our site at the time! <a href="/logs">Find captian's log here!</a>`)
+    res.status(404).redirect("/logs")
 }
 })
 
@@ -39,7 +46,30 @@ res.status(202).json(log)
  logs.post("/", (req, res) => {
     const newLog = req.body
     logsArray.push(newLog)
-    res.status(202).json({success: true, payload: logsArray})
+    res.status(202).json(logsArray)
+ })
+
+ //Delete
+ logs.delete("/:id", (req, res) => {
+    const {id} = req.params;
+    if (logsArray[id]) {
+        const removedLog = logsArray.splice(id, 1);
+        res.status(202).json(removedLog)
+    }else {
+        res.status(404).json({error: `No log with that ${id}`})
+    }
+ })
+
+ //Update
+ logs.put("/:id", (req, res) => {
+    const {id} = req.params;
+    const updatedLog = req.body;
+    if (logsArray[id]) {
+        logsArray[id] = updatedLog
+        res.status(202).json(logsArray)
+    }else {
+        res.status(404).json({error: `No log with that ${id}`})
+    }
  })
 
 module.exports = logs
