2790d93b4ffd7ea3bbcc1f93dd296fb3598663af
USERNAME <EMAIL>
2023-04-12 20:55:45 -0400

    lab-express-rest-show-create-All Tests Pass

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ const logsController = require('./controllers/logsController');
 
 app.use(express.json())
 
-
 app.use("/logs", logsController)
+
 
 app.get('/', (req, res) => {
     res.send("welcome to the captain's log");
 });
-
 
 app.get('*', (req, res) => {
     res.status(404).json({ error: "Page not found"});
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -7,7 +7,38 @@ logs.get('/', (req, res) => {
     res.json(logsArray)
 })
 
+logs.get('/:id', (req, res) => {
+    const { id } = req.params
+    const log = logsArray[id]
+    if (log) {
+        res.json(logsArray[id])
+    } else {
+        res.redirect(302, "/*")
+    }
+})
+
+logs.post("/", (req, res) => {
+    const newLog = req.body
+    logsArray.push(newLog)
+    res.json(logsArray)
+})
+
+// Delete
+logs.delete('/:id', (req, res) => {
+    const { id } = req.params
+
+    const deletedLog = logsArray.splice(id, 1)
+    res.status(202).json(deletedLog)  
+})
+
+
+
+
+
 module.exports = logs;
+
+
+
 
 // CREATE
 // bookmarks.post("/", (req, res) => {
