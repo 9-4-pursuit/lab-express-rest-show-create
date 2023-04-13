43fb9fa3fbab380a420b749e58004f08f59a73f5
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-04-12 21:15:43 -0400

    passed all 6

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const logsController = require("./controllers/logsController")
 
+app.use(express.json())// parses incoming data
+app.use("/logs", logsController) 
+
 
 app.get("/", (req, res) => {
 res.send ("welcome to captain's log")
 })
 
 
-app.use("/logs", logsController)
 
 app.get("*", (req, res) => {
     res.status(404).json()
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -1,20 +1,46 @@
-// this is where we write the logs routes -- we dont have a dtabase so we use the models
-const express = require("express");
-const logs = express.Router();
 const logsArray = require("../models/log.js");
+const express = require("express");
 
+const logs = express.Router();
+
+//GET HOME
 logs.get("/", (req, res) => {
   res.json(logsArray);
 });
 
-logs.get("/:id", (req))
+//GET INDEX
+logs.get("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  const log = logsArray[arrayIndex];
 
+  if (log) {
+    res.json(log);
+  } else {
+    res.redirect("/logs");
+  }
+});
+
+ // CREATE
+ //! invoking end helps to 
 logs.post("/", (req, res) => {
   const newLog = req.body;
-
   logsArray.push(newLog);
+  res.status(303).header( "/logs").end();
+});
 
-  res.status(200).json({ success: true, payload: newLog });
+// UPDATE
+logs.put("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  const updatedLog = req.body;
+  logsArray[arrayIndex] = updatedLog;
+  res.status(303).header( "/logs/").end();
+});
+
+//DELETE
+logs.delete("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+  logsArray.splice(arrayIndex, 1);
+  res.status(303).header( "/logs").end();
 });
 
 module.exports = logs;
