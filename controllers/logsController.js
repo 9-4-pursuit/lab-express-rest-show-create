6004f0d18df4b65b0a9a9d45e1a658753727e0ef
Diegocarrero <diegocar2413@gmail.com>
2023-04-12 20:06:31 -0400

    Pass all tests

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -29,4 +29,29 @@ logs.post('/', (req, res) => {
     }
 })
 
+// update
+logs.put('/', (req, res) => {
+    const { id } = req.params;
+    const updatedLog = req.body;
+
+    if (logsArray[id]) {
+        logsArray[id] = updatedLog;
+        res.status(303).json(logsArray[id])
+    } else {
+        res.status(404).json({success: false, error: `There was no log with the id of ${id}`})
+    }
+})
+
+// delete
+logs.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (logsArray[id]) {
+        const deletedLog = logsArray.splice(id, 1)
+        res.status(202).json(deletedLog)
+    } else {
+        res.json({success: false, error: `There is no log at id ${id}`})
+    }
+})
+
 module.exports = logs;
