04f85f8ec50a8ab2da5f83bfffa9597585b20a92
SuperNinjaEv <evan0210@gmail.com>
2023-04-12 20:46:19 -0400

    All tests passed.  Beginning bonuses

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -12,6 +12,7 @@ logs.get("/", (req, res) => {
     }
 });
 
+// INDIVIDUAL LOG
 logs.get("/:id", (req, res) => {
     const { id } = req.params;
     const logs = logsArray[id];
@@ -19,8 +20,47 @@ logs.get("/:id", (req, res) => {
     if (logs) {
         res.status(202).json(logs);
     } else {
-        res.redirect("/*");
+        res.redirect(302, "/*");
     }
 })
+
+// CREATE
+logs.post("/", (req, res) => {
+    const newLog = req.body;
+    logsArray.push(newLog);
+    res.status(202).json({ success: true, payload: logsArray });
+    
+    // if (newLog.captainName === undefined) {
+    //     logsArray.push(newLog);
+    //     res.status(202).json({ success: true, payload: logsArray });
+    // } else {
+    //     res.status(404).json({ success: false, error: "Not created successfully" });
+    // }
+});
+
+// UPDATE
+logs.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const updatedLog = req.body;
+
+    if (logsArray[id]) {
+        logsArray[id] = updatedLog;
+        res.status(202).json({ success: true, payload: logsArray[id] });
+    } else {
+        res.status(404).json({ success: false, error: `There is no log with the id of ${id}` });
+    }
+})
+
+// DELETE
+logs.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    if (logsArray[id]) {
+        const deletedLog = logsArray.splice(id, 1);
+        res.status(202).json({ success: true, payload: deletedLog });
+    } else {
+        res.status(404).json({ success: false, error: `There is no log with the id of ${id}` });
+    }
+});
 
 module.exports = logs;
