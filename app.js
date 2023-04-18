bc69cd986d50cecccecefbe154acafa9381b0ee3
USERNAME <EMAIL>
2023-04-18 15:06:12 -0400

    mods made

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const logs = require('./controllers/logsController')
 const app = express();
+const cors = require ("cors");
 const logsController = require('./controllers/logsController');
 
 app.use(express.json())
+app.use(cors());
 
 app.use("/logs", logsController)
 
@@ -16,4 +17,4 @@ app.get('*', (req, res) => {
     res.status(404).json({ error: "Page not found"});
 });
 
-module.exports = app;
+module.exports = app;  
--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,47 +2,46 @@ const express = require('express');
 const logs = express.Router();
 const logsArray = require('../models/log.js')
 
-// index
-logs.get('/', (req, res) => {
-    res.json(logsArray)
-})
-
-logs.get('/:id', (req, res) => {
-    const { id } = req.params
-    const log = logsArray[id]
-    if (log) {
-        res.json(logsArray[id])
-    } else {
-        res.redirect(302, "/*")
-    }
-})
-
 logs.post("/", (req, res) => {
     const newLog = req.body
     logsArray.push(newLog)
     res.json(logsArray)
 })
 
+
+// index
+logs.get('/', (req, res) => {
+    res.json(logsArray)
+})
+
+// Single Log
+logs.get('/:id', (req, res) => {
+    const { id } = req.params
+    const log = logsArray[id]
+    if (log) {
+        res.send(logsArray[id])
+    } else {
+        res.status(404).redirect("*")
+    }
+})
+
+
 // Delete
 logs.delete('/:id', (req, res) => {
     const { id } = req.params
-
     const deletedLog = logsArray.splice(id, 1)
     res.status(202).json(deletedLog)  
 })
 
-
-
-
+// CREATE
+logs.post("/newlog", (req, res) => {
+    const newLog = req.body;
+    logsArray.push(newLog);
+    res.status(201).json({ success: true, payload: newLog });
+  });
 
 module.exports = logs;
 
 
 
 
-// CREATE
-// bookmarks.post("/", (req, res) => {
-//     const newBookmark = req.body;
-//     bookmarksArray.push(newBookmark);
-//     res.status(201).json({ success: true, payload: newBookmark });
-//   });
